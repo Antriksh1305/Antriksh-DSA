dc77d934773462f9e8ca23118cad10fd7e671820
Antriksh Gupta <100402656+Antriksh1305@users.noreply.github.com>
2023-01-28 23:07:19 +0530

    Create main.cpp

---
+++ Geeks for Geeks Questions/The Celebrity Problem/main.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+
+/*
+GFG Question:  The Celebrity Problem (important)
+Time Complexity: O(N^2)
+Space Complexity: O(1)
+*/
+using namespace std;
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        int k = 0;
+        while(k < n){
+            int j = k;
+            int flag = 1;
+            for(int i = 0; i < n; i++){
+                
+                if(i < k){
+                    if(M[i][j] == 1 and M[j][i] == 0) flag = 1;
+                    else{
+                        flag = 0;
+                        break;
+                    }
+                }
+                else if(i > k){
+                    if(M[i][j] == 1 and M[j][i] == 0) flag = 1;
+                    else{
+                        flag = 0;
+                        break;
+                    }
+                }
+            }
+            if(flag == 1) return k;
+            k++;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+// } Driver Code Ends
