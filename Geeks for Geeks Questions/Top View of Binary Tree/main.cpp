e1a70c01293aec6027c65bb3bca173607a6fa7ff
Antriksh Gupta <100402656+Antriksh1305@users.noreply.github.com>
2023-07-26 01:07:24 +0530

    Create main.cpp

---
+++ Geeks for Geeks Questions/Top View of Binary Tree/main.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        
+        unordered_map <int,int> res;
+        int max = 0, min = 0;
+        queue <pair<Node*,int>> q;
+        
+        q.push(make_pair(root,0));
+        
+        while(!q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            
+            Node* tempNode = temp.first;
+            int hd = temp.second;
+            
+            if(res[hd] == 0){
+                res[hd] = tempNode->data;
+            }
+            
+            if(tempNode->left){
+                q.push({tempNode->left,hd-1});
+            }
+            if(tempNode->right){
+                q.push({tempNode->right,hd+1});
+            }
+            
+            if(hd > max) max = hd;
+            else if(hd < min) min = hd;
+        }
+        
+        for(int i = min; i <= max; i++){
+            ans.push_back(res[i]);
+        }
+        
+        return ans;
+    }
+};
